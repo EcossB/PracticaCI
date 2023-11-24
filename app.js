a004c3f1cda8f1e878efae159d62d967f948a6d0
EcossB <erwingcoss21@hotmail.com>
2023-11-24 17:24:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
+
+module.exports = app; // Exportar la aplicación para pruebas
